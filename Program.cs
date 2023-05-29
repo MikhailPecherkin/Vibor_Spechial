ad4ec2ba9d476fb1e16d49fca0b69f6efc665f66
MikhailPecherkin <m.pechiorkin@yandex.ru>
2023-05-29 16:36:59 +0300

    Program is ready

--- Program.cs
+++ Program.cs
@@ -2,3 +2,18 @@
 System.Console.Write("Введите элементы через пробел: ");
 string text = Convert.ToString(Console.ReadLine())!;
 
+// - Разделение по пробелу
+string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+// - Прохождение по всем элементам
+foreach (string rez in words)
+{
+    // - Поиск слов менее 4 символов
+    if (rez.Length < 4)
+    {
+    // - Вывод интересующих слов   
+       Console.Write($"{rez} ");
+    }
+}
+// - отступ вниз по завершении
+System.Console.WriteLine("");
